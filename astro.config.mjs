142d37a9d815ec6d5282d86f1b211594ef928248
Carter Rabasa <carter.rabasa@gmail.com>
2023-02-08 16:00:59 -0800

    switching to functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
